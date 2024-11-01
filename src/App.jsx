1cdd64eba493f4553f85468e9ae567a5ffd9face
Mesakh Lolo <91011194+mesakhlolo@users.noreply.github.com>
2024-11-01 21:39:19 +0700

    setup bootstrap and react-bootstrap for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
-  return <div>Test</div>;
+  return (
+    <section>
+      <div>
+        <h1>Test Bootstrap</h1>
+        <Button variant="success">Success Button</Button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
