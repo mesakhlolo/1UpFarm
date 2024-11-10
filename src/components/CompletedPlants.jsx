d7bd7c88404c45b34b81f489100a7ddaa92d71ed
sh1z1a <h4sn44ziz4h@gmail.com>
2024-11-11 01:01:32 +0700

    tambah page profile

--- src/components/CompletedPlants.jsx
+++ src/components/CompletedPlants.jsx
@@ -6,7 +6,7 @@ const CompletedPlants = ({ plants }) => {
   return (
     <div>
       <h2 className="fontPoppins fw-bold h5 font-weight-bold mb-3 ms-4">Tanaman Selesai</h2>
-      <div className="scrollable row mb-4 mx-4" style={{ maxHeight: '400px', overflowY: 'auto' }}>
+      <div className="scrollable row mb-4 mx-4" style={{ maxHeight: '400px' }}>
         {plants.map((plant, index) => (
           <div key={index} className="col-md-4 mb-4 d-flex justify-content-center">
             <TanamanCard name={plant.name} status={plant.status} condition={plant.condition} image={plant.image} />
